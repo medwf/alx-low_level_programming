b33a770f35036aeb254391c3cc974d86c82b963e
medwf <med.wf95@gmail.com>
2023-06-15 07:07:50 -0700

    all task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,18 @@
-#include <unistd.h>
-#include <stdio.h>
+#include<unistd.h>
+
 /**
-* main - Entry point
-*
-*Description: print string zithout using printf and puts
-*
-*Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Description: print a quote using write function
+ *    ssize_t write(int fd, const void *buf, size_t count);
+ *
+ * Return: This time we return an error 1.
 */
+
 int main(void)
 {
-int i;
-char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1, string, 59);
-return (1);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
 }
