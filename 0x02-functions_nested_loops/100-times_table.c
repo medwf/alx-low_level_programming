a32f578a5c94a700261aee3d1a65204d47fcd07a
medwf <med.wf95@gmail.com>
2023-06-21 11:24:38 +0100

    add Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,34 +1,39 @@
 #include "main.h"
 /**
  * print_times_table - prints the n print times table, starting with 0
- *
+ *@n: multiplication number.
  * Return: No return.
  */
 void print_times_table(int n)
 {
-	int line, coluns;
+	int line, coluns, mul;
 
-	for (line = 0 ; line <= n ; line++)
+	if (n >= 0 && n <= 15)
 	{
-		if (n > 15 || n < 0)
-			continue;
-		_putchar(48);
-		for (coluns = 1 ; coluns <= n ; coluns++)
+		for (line = 0 ; line <= n ; line++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
-			if ((line * coluns) > 99)
-				_putchar((line * coluns) / 100 + 48);
-			else if ((line * coluns) > 9)
-				_putchar((line * coluns) / 10 + 48);
-			else
+			_putchar(48);
+			for (coluns = 1 ; coluns <= n ; coluns++)
 			{
+				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
+
+				mul = line * coluns;
+
+				if (mul <= 9)
+					_putchar(' ');
+				if (mul <= 99)
+					_putchar(' ');
+				if (mul > 99)
+				{
+					_putchar((mul / 100) + 48);
+					_putchar((mul / 10) + 38);
+				}
+				else if (mul <= 99 && mul >= 10)
+					_putchar((mul / 10) + 48);
+				_putchar((line * coluns) % 10 + 48);
 			}
-			_putchar((line * coluns) % 10 + 48);
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
