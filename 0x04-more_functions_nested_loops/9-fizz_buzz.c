472620367ab45fa43741a3213bd87bd7902a5f18
medwf <med.wf95@gmail.com>
2023-06-22 15:09:42 +0100

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 1 ; number <= 100 ; number++)
+	{
+		if (number % 3 == 0 && number % 5 == 0)
+			printf("FizzBuzz ");
+		else if (number % 3 == 0)
+			printf("Fizz ");
+		else if (number % 5 == 0)
+		{
+			printf("Buzz");
+			if (number != 100)
+				printf(" ");
+		}
+		else
+			printf("%d ", number);
+	}
+	printf("\n");
+	return (0);
+}
