b82d1535dda37b0685a035caca4a8c0e8eee2f74
medwf <med.wf95@gmail.com>
2023-06-26 15:59:42 +0100

    tast 0

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_number - prints an integer using putchar.
+ *
+ *@n: number
+ *Return: No return.
+ */
+ void print_number(int n)
+ {
+	 if ()
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - change value.
+ *
+ *@n: pointer to n.
+ *Return: No return.
+ */
+ void reset_to_98(int *n)
+ {
+	 *n = 98;
+ }
