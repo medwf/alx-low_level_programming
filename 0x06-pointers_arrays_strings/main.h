e8d612f74929620433b51b781c7b3c37de09954b
medwf <med.wf95@gmail.com>
2023-07-01 12:51:46 +0100

    all task

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,8 @@
 char *_strcat(char *dest, char *src);
+void print_number(int n);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size)
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
