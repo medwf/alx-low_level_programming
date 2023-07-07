cc725026d7e47c596cb73d26dec0499ec9a938ae
medwf <med.wf95@gmail.com>
2023-07-07 20:31:37 +0100

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives. 
+ *@argc: The number of arguments.
+ *@argv: an array of pointers to the arguments.
+ *Return: Always to 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0 ; i < argc ; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
