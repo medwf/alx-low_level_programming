21ea0775b7dfd17e61ac0f548287132da5fdf012
med.wf <med.wf95@gmail.com>
2023-07-19 16:21:00 +0100

    fixed output task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,9 +19,9 @@ int main(int argc, char *argv[])
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	op = argv[2];
+	op = argv[2][0];
 
-	if (argv[2][1] != '\0' || (op != '+' && op != '-' &&
+	if (argv[2][1] != '\0' && (op != '+' && op != '-' &&
 		op != '/' && op != '*' && op != '%'))
 	{
 		printf("Error\n");
