0d5e6d0f8f12e8d41d23c4a942bacadea9f345ee
med.wf <med.wf95@gmail.com>
2023-07-31 17:11:04 +0100

    fixed task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,15 +3,17 @@
 /**
  * print_listint - a function that prints all the elements of a listint_t list.
  * @h: pointer to head.
-*/
+ * Return: the number of nodes.
+ */
 size_t print_listint(const listint_t *h)
 {
-    int num_node = 0;
-    while (h)
-    {
-        printf("%d\n", h->n);
-        num_node++;
-        h = h->next;
-    }
-    return (num_node);
+	int num_node = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num_node++;
+		h = h->next;
+	}
+	return (num_node);
 }
