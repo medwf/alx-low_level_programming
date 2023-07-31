469220e16b2926682557da987dcc4fe5417eb3cf
med.wf <med.wf95@gmail.com>
2023-07-31 17:54:47 +0100

    fixed task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 -  a function that frees a listint_t list.
+ * @head: pointer to the first linked list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *free_node = NULL;
+
+	free_node = *head;
+	while (free_node->next)
+	{
+		free_node = free_node->next;
+		free(*head);
+		*head = free_node;
+	}
+	free(free_node);
+	*head = NULL;
+}
