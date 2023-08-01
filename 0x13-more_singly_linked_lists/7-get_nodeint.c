84d8765e8b8e99672a9fdd3a34d694f4829ce3f5
med.wf <med.wf95@gmail.com>
2023-08-01 11:17:33 +0100

    fixed task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,8 +10,15 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *move_node = NULL;
+	unsigned int num_node = 0;
 
-	if (!head)
+	move_node = head;
+	while (move_node->next)
+	{
+		move_node = move_node->next;
+		num_node++;
+	}
+	if (!head || (num_node < index))
 		return (NULL);
 	move_node = head;
 	while (index)
