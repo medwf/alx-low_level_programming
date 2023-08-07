14489392c70ee0193018c60ebbe468e26dd093cd
med.wf <med.wf95@gmail.com>
2023-08-07 23:59:40 +0100

    try to make advance

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,35 +1,41 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <elf.h>
-
 /**
  * main - Entry points
  * @argc: the number of argument.
  * @argv: an arrays of string.
- * Return: 
+ * Return: 98 0r 0.
 */
 int main(int argc, char **argv)
 {
-    int file, read_file;
-    Elf64_Ehdr reader;
+	int file, read_file;
+	char stored[64];
+	/*Elf64_Ehdr *header;*/
 
-    if (argc != 2)
-    {
-        dprintf(STDERR_FILENO, "Usage : %s elf_file\n", argv[0]);
-        exit(98);
-    }
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage : %s elf_file\n", argv[0]);
+		exit(98);
+	}
 
-    file = open(argv[1], O_RDONLY);
-    
+	file = open(argv[1], O_RDONLY);
 	if (file == -1)
-    {
-        dprintf(STDERR_FILENO, "Error can't open this file %s\n", argv[1]);
-        exit(98);
-    }
-    
-    close(file);
-    return (0);
+	{
+		dprintf(STDERR_FILENO, "Error: can't open this file %s\n", argv[1]);
+		exit(98);
+	}
+	read_file = read(file, stored, 64);
+	if (read_file == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't read from this file %s\n", argv[1]);
+		exit(98);
+	}
+	if (stored[0] != 0x7f || stored[1] != 'E' || stored[2] != 'L' || stored[3] !=
+		 'F')
+	{
+		fprintf(stderr, "%s is not an ELF file\n", argv[1]);
+		exit(98);
+	}
+	/* i don't know how can i print it yet */
+	close(file);
+	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
-
+#include <elf.h>
 /* include prototype */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
