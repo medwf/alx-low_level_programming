f9d29c1f4265b91a7202344859e8d6b1862d2c13
med.wf <med.wf95@gmail.com>
2023-09-14 13:40:31 +0100

    fix betty

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,7 +4,7 @@
  * dlistint_len - a function that returns the number of elements
  *                    in a linked dlistint_t list
  * @h: a pointer to linked list.
- * Return : the number of nodes
+ * Return: the number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
