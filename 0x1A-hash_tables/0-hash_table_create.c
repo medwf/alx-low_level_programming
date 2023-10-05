5b285b010c7925f82de89c8f1f2e1cf245c608f3
med.wf <med.wf95@gmail.com>
2023-10-05 16:38:29 +0100

    fix task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	/* if new_table not created return NULL*/
-	new_table = malloc(sizeof(hash_node_t));
+	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
 		return (NULL);
 
