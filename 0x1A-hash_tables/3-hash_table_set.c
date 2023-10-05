dc68d93c91a66a644ff1abcc34edc4fe00a38bc2
med.wf <med.wf95@gmail.com>
2023-10-05 17:25:19 +0100

    fix error

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -35,7 +35,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = NULL;
 	new_node->next = ht->array[idx];
 	ht->array[idx] = new_node;
 	return (1);
